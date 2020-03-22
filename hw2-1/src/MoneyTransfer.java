c26e07ceac5ec69a1cfb715ef7c2ed31c179088b
Ekaterina <rymareva_ea@mail.ru>
2020-03-22 23:12:09 +0500

    Added all file

---
+++ hw2-1/src/MoneyTransfer.java
@@ -0,0 +1,8 @@
+public class MoneyTransfer {
+    public static void main(String[] args) {
+        int CurrentBalance= 2_000_000_000;
+        int TransferAmount = 500_000_000;
+        int TotalBalance = CurrentBalance + TransferAmount;
+        System. out. println("Total balance = " + TotalBalance);
+    }
+}
